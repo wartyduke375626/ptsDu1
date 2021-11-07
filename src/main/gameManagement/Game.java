2cd4a47ead12fa47f7f1c85dfd3ed0d0cb24eba9
Dennis Vita <vita3@uniba.sk>
2021-11-07 15:27:49 +0100

    IsGameOver
    removed isgameOver atribute in Game class, isgameOver method now calls
    endGameStrategy.isGameOver()

--- src/main/gameManagement/Game.java
+++ src/main/gameManagement/Game.java
@@ -4,29 +4,27 @@ import gameComponents.*;
 
 public class Game implements GameInterface {
 
-    private TurnInterface turnInterface;
+    private final TurnInterface turnInterface;
     private boolean isPlayCardPhase;
-    private EndGameStrategy endGameStrategy;
-    private boolean isGameOver;
+    private final EndGameStrategy endGameStrategy;
 
     public Game(TurnInterface turnInterface, EndGameStrategy endGameStrategy) {
         isPlayCardPhase = false;
         this.turnInterface = turnInterface;
         this.endGameStrategy = endGameStrategy;
-        isGameOver = endGameStrategy.isGameOver();
         endTurn();
     }
 
     @Override
     public boolean playCard(int handIndex) {
-        if (isGameOver) return false;
+        if (isGameOver()) return false;
         if (isPlayCardPhase) return turnInterface.playCard(handIndex);
         return false;
     }
 
     @Override
     public boolean endPlayCardPhase() {
-        if (isGameOver) return false;
+        if (isGameOver()) return false;
         if (!isPlayCardPhase) return false;
         isPlayCardPhase = false;
         return true;
@@ -34,24 +32,23 @@ public class Game implements GameInterface {
 
     @Override
     public boolean buyCard(GameCardType gameCardType) {
-        if (isGameOver) return false;
+        if (isGameOver()) return false;
         if (!isPlayCardPhase) return turnInterface.buyCard(gameCardType);
         return false;
     }
 
     @Override
     public boolean endTurn() {
-        if (isGameOver) return false;
+        if (isGameOver()) return false;
         if (isPlayCardPhase) return false;
         isPlayCardPhase = true;
         turnInterface.newTurn();
-        isGameOver = endGameStrategy.isGameOver();
         return true;
     }
 
     @Override
     public boolean isGameOver() {
-        return isGameOver;
+        return endGameStrategy.isGameOver();
     }
 
 }
